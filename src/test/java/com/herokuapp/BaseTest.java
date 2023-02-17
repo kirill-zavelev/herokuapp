6a3e1eab5bed6631880f0b33682796170eca0c4b
kirill-zavelev <kiri4by@gmail.com>
2023-02-17 22:44:25 +0300

    Adding assertJ dependency and wait to use in homeworks

--- src/test/java/com/herokuapp/BaseTest.java
+++ src/test/java/com/herokuapp/BaseTest.java
@@ -3,6 +3,7 @@ package com.herokuapp;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,6 +12,7 @@ import java.time.Duration;
 public class BaseTest {
 
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
@@ -18,6 +20,7 @@ public class BaseTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterMethod(alwaysRun = true)
